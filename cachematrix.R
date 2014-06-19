8f4980b19cd25f7ec337b52fdcff5b08551b9aff
Haris Skiadas <cskiadas@gmail.com>
2014-06-19 14:15:04 -0400

    added comments and alternate version

--- cachematrix.R
+++ cachematrix.R
@@ -3,6 +3,11 @@
 
 ## Write a short comment describing this function
 
+## X=makeCacheMatrix() creates a reassignable matrix that is meant to hold its inverse matrix
+## X$set(x) allows us to reset the stored matrix
+## X$get() returns the stored matrix
+## getinverse and setinverse shouldn't be used directly, use cacheSolve(X) instead
+
 makeCacheMatrix <- function(x = matrix()) {
     inv <- NULL
     set <- function(y) {
@@ -21,6 +26,16 @@ makeCacheMatrix <- function(x = matrix()) {
 
 ## Write a short comment describing this function
 
+## Computes and caches the inverse of a matrix created via makeCacheMatrix
+## Usage: X = makeCacheMatrix()
+##        cacheSolve(X)     ## Computes, caches and returns the inverse of X
+##        cacheSolve(X)     ## uses the cached value
+##        X.set(y)          ## Resets the matrix stored in X, and clears the cache
+##        cacheSolve(X)     ## Computes, caches and returns the inverse of the new matrix in X
+##        cacheSolve(X)     ## uses the new cached value
+
+## See after the code for this function if interested in an alternative way to do the caching
+
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
         inv <- x$getinverse()
@@ -33,3 +48,38 @@ cacheSolve <- function(x, ...) {
         x$setinverse(inv)
         inv
 }
+
+### In my opinion, a more elegant solution exists, where cacheSolve is incorporated
+### into makeCacheMatrix's getinverse. Sample code follows, commented so it does not execute.
+### The idea is that getinverse simply calls an internal function called f. That function defaults to
+### a function called getinverseDefault, which when executed computes the inverse of the matrix,
+### and then replaces the function f with a version that just accesses this computed inverse.
+### Subsequent calls to getinverse will just use this overwritten f.
+### A call to X$set resets f back to this default.
+###
+### Only one function needed with this technique
+
+# alternateCacheMatrix <- function(x = matrix()) {
+#     f <- getinverseDefault <- function() {
+#         inv <- solve(x)
+#         f <<- function() { message("cached"); inv }
+#         inv
+#     }
+#     set <- function(y) {
+#             x <<- y
+#             f <<- getinverseDefault
+#     }
+#     get <- function() x
+#     getinverse <- function() { f() }
+#     list(set = set, get = get,
+#          getinverse = getinverse)
+# }
+# 
+# y = alternateCacheMatrix(matrix(rnorm(16), 4))
+# y$get()
+# y$getinverse()
+# y$getinverse()
+# y$set(matrix(rnorm(16), 4))
+# y$get()
+# y$getinverse()
+# y$getinverse()
